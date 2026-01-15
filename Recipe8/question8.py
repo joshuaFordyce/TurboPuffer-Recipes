8c68ce70c09e67e6a5394d72b4ba1961433544d8
joshua fordyce880 <jfordyce880@gmail.com>
2026-01-15 16:13:09 -0500

    Remove API key from Turbopuffer initialization
    Removed API key from configuration for security.

--- Recipe8/question8.py
+++ Recipe8/question8.py
@@ -6,7 +6,7 @@ import time
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 model = SentenceTransformer('all-MiniLM-L6-v2')
 tpuf = turbopuffer.Turbopuffer(
-    api_key="tpuf_zPDvexzzKn0cfGh3eUNJbw0YgVHQFFg9",
+    api_key="",
     region="gcp-us-central1"
 )
 
